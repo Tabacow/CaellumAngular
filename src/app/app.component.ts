bd674ba950e5f794d765bb2e13eb674ced814e13
Guilherme K Tabacow <guilhermetabacow@gmail.com>
2020-11-25 15:11:23 -0300

    cmail2

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  private _isNewEmailFormOpen = false
+
+  get isNewEmailFormOpen() {
+    return this._isNewEmailFormOpen;
+  }
+
+  toggleNewEmailForm() {
+    this._isNewEmailFormOpen = !this.isNewEmailFormOpen
+  }
+}
